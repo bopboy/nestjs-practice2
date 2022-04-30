94bcd87b1fcc4b7ecbd019b9d7f00db64764cad7
bopboy <39391891+bopboy@users.noreply.github.com>
2022-05-01 07:50:43 +0900

    26 특정 게시물 삭제 요청 에러 처리

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -22,7 +22,8 @@ export class BoardsService {
         return found
     }
     deleteBoard(id: string): void {
-        this.boards = this.boards.filter(board => board.id !== id)
+        const found = this.getBoardById(id)
+        this.boards = this.boards.filter(board => board.id !== found.id)
     }
     updateBoardStatus(id: string, status: BoardStatus): Board {
         const board = this.getBoardById(id)
