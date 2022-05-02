91caaad8eadcf0de608b85c505c4ae827754220f
bopboy <39391891+bopboy@users.noreply.github.com>
2022-05-03 06:57:12 +0900

    51 게시물 생성시 유저 정보 추가

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -5,6 +5,7 @@ import { CreateBoardDto } from './dto/create-board.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BoardRepository } from './board.repository';
 import { Board } from './board.entity';
+import { User } from 'src/auth/user.entity';
 
 @Injectable()
 export class BoardsService {
@@ -18,8 +19,8 @@ export class BoardsService {
         return this.boardRepository.find()
     }
     // getAllBoards(): Board[] { return this.boards }
-    createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
-        return this.boardRepository.createBoard(createBoardDto)
+    createBoard(createBoardDto: CreateBoardDto, user: User): Promise<Board> {
+        return this.boardRepository.createBoard(createBoardDto, user)
     }
     // createBoard(createBoardDto: CreateBoardDto) {
     //     const { title, description } = createBoardDto
